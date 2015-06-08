e5861c2169a2d27d065ecf0d04e9cc7187d68c34
colinsr <colinsr@me.com>
2015-06-08 16:13:53 -0400

    delete test fil

--- src/test.js
+++
@@ -1,6 +0,0 @@
-export var mytest = [
-  { answer: "Ohio.", question: "Columbus?" },
-  { answer: "Indiana.", question: "Indianapolis?" },
-  { answer: "Michigan.", question: "Lansing?" },
-  { answer: "Kentucky.", question: "Lexington?" }
-];
